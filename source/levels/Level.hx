f01eadd5637d5aca08879188353903abd5742544
Jake Caban-Tomski <jacque006@gmail.com>
2021-01-30 12:04:18 -0700

    More level updates, correct scaling

--- source/levels/Level.hx
+++ source/levels/Level.hx
@@ -1,22 +1,28 @@
 package levels;
 
+import objects.WallSegment;
+import flixel.group.FlxSpriteGroup;
 import flixel.tile.FlxTilemap;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 
 class Level {
 
-	public var testLayer:FlxTilemap;
+    public var testLayer:FlxTilemap;
+    public var walls:FlxSpriteGroup;
 
 	public function new(level:String) {
         var loader = new FlxOgmo3Loader(AssetPaths.levels__ogmo, level);
-        testLayer = loader.loadTilemap(AssetPaths.filler__png, "test");
+        testLayer = loader.loadTilemap(AssetPaths.test__png, "walls");
 
+        walls = new FlxSpriteGroup();
 		// loader.loadEntities((entityData) -> {
         //     /*
         //      * entityData.name
         //      * entityData.x
         //      * entityData.y
         //      */
-		// }, "test");
+        //      var ws = new WallSegment(entityData.x, entityData.y);
+        //      walls.add(ws);
+		// }, "walls");
 	}
 }
