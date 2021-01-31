8542cec9b9694e00ee49cd933d29dea79f4a90fe
Logan <monday.hopscotch@gmail.com>
2021-01-30 23:20:53 -0700

    get metrics set up. max speed metric reporting

---
+++ source/metrics/Trackers.hx
@@ -0,0 +1,18 @@
+package metrics;
+
+import flixel.FlxG;
+import com.bitdecay.analytics.Bitlytics;
+
+class Trackers {
+	public static var MAX_SPEED = 0;
+	private static var SPEED_BUCKET_SIZE = 20;
+
+	public static function checkSpeed(speed:Float) {
+		// save a bunch of reports and just check reasonable increments
+		var bracket = Math.floor(speed / SPEED_BUCKET_SIZE) * SPEED_BUCKET_SIZE;
+		if (bracket > MAX_SPEED) {
+			MAX_SPEED = bracket;
+			Bitlytics.Instance().Queue(Metrics.MAX_SPEED, MAX_SPEED);
+		}
+	}
+}
