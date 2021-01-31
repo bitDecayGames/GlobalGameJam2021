1e62dac16539f604fae823fbcc9845e4a7b24b4a
mwingfield <mwingfie@gmail.com>
2021-01-30 18:37:05 -0700

    add sfx action

---
+++ source/cutscenes/actors/CloseUpSpaceShip.hx
@@ -0,0 +1,10 @@
+package cutscenes.actors;
+
+import flixel.FlxSprite;
+
+class CloseUpSpaceShip extends FlxSprite {
+	public function new() {
+		super();
+		loadGraphic(AssetPaths.FLStudioLogo__png);
+	}
+}
